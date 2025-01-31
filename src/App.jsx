ac20a3b4e9e3b8b0aaeb009bbd82b2fb816997ec
Unika1 <guragainaruna@gmail.com>
2025-01-31 17:21:57 +0545

    React Configuration of User Registration

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import React, { Suspense, useState } from 'react'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
+import './public/UserRegistration.css'
+
 function App() {
   const MainPage = React.lazy(() => import("./public/MainPage.jsx"));
+  const UserRegistration = React.lazy(() => import("./public/UserRegistration.jsx"));
+  const PasswordRecovery = React.lazy(() => import("./private/PasswordRecovery.jsx"))
 
   return (
     <Router>
       <Suspense fallback={<div>loading...</div>}>
       <Routes>
         <Route path="/" element={<Navigate to ="/MainPage"/>}></Route>
+        <Route path="/userregistration" element={<UserRegistration/>}></Route>
+        <Route path="/passwordrecovery" element={<PasswordRecovery/>}></Route>
       </Routes>
       </Suspense>
     </Router>
