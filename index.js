361732ef83a9c803e226becd73cf6e801517b34e
Unika1 <guragainaruna@gmail.com>
2025-03-04 17:58:38 +0545

    Changes

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true })); 
 
 app.get('/', (req, res) => {
   res.send("Welcome to Profix API");
